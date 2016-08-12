6d08650d8f103085a0154a6356b110099ab4ea9c
shyssirixius <ivmikvik@gmail.com>
2016-08-12 04:30:32 +0300

    add cart_get_numb_of_items.js

--- public/app.js
+++ public/app.js
@@ -18,6 +18,23 @@
 		var x = window.localStorage.getItem(key);
 		x = x * 1 + 1;
 		window.localStorage.setItem(key, x);
+
+		alert('Items in your cart: ' + cart_get_numb_of_items())
  	}
 
-	
+	function cart_get_numb_of_items() {
+
+		var cnt = 0;
+		for (var i = 0; i < window.localStorage.length; i++) 
+			{
+				var key = window.localStorage.key(i); //getting key
+				var value = window.localStorage.getItem(key); //getting value ruby way: hh[key] = x
+
+				if (key.indexOf('product_') == 0)
+				{
+					cnt = cnt + value * 1;
+				}
+			}
+
+			return cnt;
+	}
