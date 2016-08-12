cc6918783103323b0514b8731e1272b488150b9b
shyssirixius <ivmikvik@gmail.com>
2016-08-12 04:11:07 +0300

    initial commit

---
+++ public/app.js
@@ -0,0 +1,23 @@
+
+
+	function something() 
+	{
+		var x = window.localStorage.getItem('bbb'); // x = hh['bbb']
+
+		x = x * 1 + 1; //string to integer x = x + 1
+
+		window.localStorage.setItem('bbb', x); // hh['bbb'] = x
+
+		alert(x);
+	}
+
+	function add_to_cart(id) 
+	{
+		var key = 'product_' + id;
+		
+		var x = window.localStorage.getItem(key);
+		x = x * 1 + 1;
+		window.localStorage.setItem(key, x);
+ 	}
+
+	
